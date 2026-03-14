71fa19cae8c0b11f7f923ef664ab9b91144ecc2b
Matthias Buehlmaier <buehlmaier@hku.hk>
2026-03-14 19:23:07 +0800

    build: add tools.build + deps-deploy + GitHub Actions release workflow

---
+++ build.clj
@@ -0,0 +1,44 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'com.github.clojure-finance/edgarjure)
+(def version "0.1.0")
+(def class-dir "target/classes")
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     @basis
+                :src-dirs  ["src"]
+                :scm       {:url                 "https://github.com/clojure-finance/edgarjure"
+                            :connection          "scm:git:git://github.com/clojure-finance/edgarjure.git"
+                            :developerConnection "scm:git:ssh://git@github.com/clojure-finance/edgarjure.git"
+                            :tag                 (str "v" version)}
+                :pom-data  [[:description
+                             "A Clojure library for SEC EDGAR ? filings, financials, and XBRL data, ready for research."]
+                            [:url "https://github.com/clojure-finance/edgarjure"]
+                            [:licenses
+                             [:license
+                              [:name "Eclipse Public License 2.0"]
+                              [:url "https://www.eclipse.org/legal/epl-2.0/"]]]
+                            [:developers
+                             [:developer
+                              [:name "clojure-finance"]]]]})
+  (b/copy-dir {:src-dirs   ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file  jar-file}))
+
+(defn install [_]
+  (jar nil)
+  (b/install {:basis     @basis
+              :lib       lib
+              :version   version
+              :jar-file  jar-file
+              :class-dir class-dir}))
